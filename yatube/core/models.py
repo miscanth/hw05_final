4d1eae3708d7f61a3b2cfa449a366da3bb78aadd
Ermachkova Julia <miscanth@gmail.com>
2023-03-06 00:26:10 +0300

    Final flake8

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class CreatedModel(models.Model):
     """Абстрактная модель. Добавляет дату создания."""
     pub_date = models.DateTimeField(
@@ -10,4 +11,4 @@ class CreatedModel(models.Model):
 
     class Meta:
         # Это абстрактная модель:
-        abstract = True 
+        abstract = True
